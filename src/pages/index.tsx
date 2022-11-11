93f6ea8bca7cd89f1e882dc94db3ceb0ede82eb7
Irving Dinh <irving.dinh@gmail.com>
2022-11-11 13:49:32 +0700

    Added something, maybe bug... 🐛 🐞 🪲

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import Error from 'next/error'
-
 export default function Page() {
-  return <Error statusCode={501} title="Not Implemented"/>
+  return (
+    <div className="p-4">
+      501 - Not Implemented
+    </div>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xs: '10px',
+        sm: '12px',
+        base: '14px',
+        lg: '16px',
+        xl: '18px',
+        '2xl': '20px',
+        '3xl': '24px',
+        '4xl': '30px',
+        '5xl': '36px',
+      }
+    },
+  },
+  plugins: [],
+}
